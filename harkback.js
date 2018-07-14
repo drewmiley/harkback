88e51aef75287f6858569040967e740eac81f1b4
Drew Miley <drewmiley92@gmail.com>
2018-07-14 18:18:41 +0100

    Adds callbackFetch

--- harkback.js
+++ harkback.js
@@ -4,17 +4,16 @@ const checkOkResponse = res => res.ok ? res : Promise.reject({status: res.status
 const getJson = res => res.json();
 const logJson = json => console.log(json);
 const logErrorStatus = err => console.error(err.status);
+
+const callbackFetch = url => {
+    return fetch(url)
+        .then(checkOkResponse)
+        .then(getJson)
+        .catch(logErrorStatus);
+}
  
-fetch('https://randomuser.me/api/')
-    .then(checkOkResponse)
-    .then(getJson)
-    .then(logJson)
-    .catch(logErrorStatus);
+callbackFetch('https://randomuser.me/api/').then(logJson);
     
-fetch('https://httpstat.us/500')
-    .then(checkOkResponse)
-    .then(getJson)
-    .then(logJson)
-    .catch(logErrorStatus);
+callbackFetch('https://httpstat.us/500').then(logJson);
 
 console.log('harkback');
