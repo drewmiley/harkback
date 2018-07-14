10acde76630ae6aacb636669b9149d7715e123d0
Drew Miley <drewmiley92@gmail.com>
2018-07-14 10:29:23 +0100

    Adds dummy http requests

--- harkback.js
+++ harkback.js
@@ -1,1 +1,11 @@
+const fetch = require('node-fetch');
+ 
+fetch('https://github.com/')
+    .then(res => res.text())
+    .then(body => console.log(body));
+ 
+fetch('https://api.github.com/users/github')
+    .then(res => res.json())
+    .then(json => console.log(json));
+
 console.log('harkback');
