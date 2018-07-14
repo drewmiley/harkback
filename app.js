438429554ce7ab8f62fbf17baf65fbfb8e336355
Drew Miley <drewmiley92@gmail.com>
2018-07-14 19:36:38 +0100

    Removes spaces

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const harkbackFetch = require('./harkbackFetch');
-    
+
 const successFetch = harkbackFetch('https://randomuser.me/api/');
 const errorFetch = harkbackFetch('https://httpstat.us/500');
- 
-successFetch();    
+
+successFetch();
 errorFetch();
