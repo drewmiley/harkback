ce8306b8699568f33553d399e2acb114256ed0ad
Drew Miley <drewmiley92@gmail.com>
2018-07-14 17:40:01 +0100

    Refactors out methods in fetch

--- harkback.js
+++ harkback.js
@@ -1,35 +1,20 @@
 const fetch = require('node-fetch');
+
+const checkOkResponse = res => res.ok ? res : Promise.reject({status: res.status})
+const getJson = res => res.json();
+const logJson = json => console.log(json);
+const logErrorStatus = err => console.error(err.status);
  
 fetch('https://randomuser.me/api/')
-    .then(response => {
-        if (response.ok) {
-            return response;
-        } else {
-            return Promise.reject({status: response.status});
-        }
-    })
-    .then(res => res.json())
-    .then(json => {
-        return console.log(json);
-    })
-    .catch(err => {
-        console.error(err.status);
-    });
+    .then(checkOkResponse)
+    .then(getJson)
+    .then(logJson)
+    .catch(logErrorStatus);
     
 fetch('https://httpstat.us/500')
-    .then(response => {
-        if (response.ok) {
-            return response;
-        } else {
-            return Promise.reject({status: response.status});
-        }
-    })
-    .then(res => res.json())
-    .then(json => {
-        return console.log(json);
-    })
-    .catch(err => {
-        console.error(err.status);
-    });
+    .then(checkOkResponse)
+    .then(getJson)
+    .then(logJson)
+    .catch(logErrorStatus);
 
 console.log('harkback');
