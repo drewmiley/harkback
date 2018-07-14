580f2f7fb4f3011af4746819978febd526dd25c4
Drew Miley <drewmiley92@gmail.com>
2018-07-14 10:30:48 +0100

    Adds error request

--- harkback.js
+++ harkback.js
@@ -7,5 +7,8 @@ fetch('https://github.com/')
 fetch('https://api.github.com/users/github')
     .then(res => res.json())
     .then(json => console.log(json));
+    
+fetch('http://domain.invalid/')
+    .catch(err => console.error(err));
 
 console.log('harkback');
