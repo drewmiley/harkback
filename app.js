9035a80587596535759c64ee66cf537ac4e685b5
Drew Miley <drewmiley92@gmail.com>
2018-07-15 01:43:53 +0100

    Adds callbacks for console.log and variable change

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const harkbackFetch = require('./harkbackFetch');
 const myEmitter = require('./myEmitter');
 
+const FETCH_SUCCESS = 'success';
+const FETCH_FAILURE = 'failure';
+const ADD_TO_NUMBER = 'add';
+
+let myNumber = 1;
+
 const successFetch = harkbackFetch('https://randomuser.me/api/');
 const errorFetch = harkbackFetch('https://httpstat.us/500');
+
+const addToNumberResult = number => () => {
+    number++;
+    console.log(number);
+}
 const events = [
-    { name: 'event', result: text => console.log(`an event occurred!${ text }`)}
+    { name: FETCH_SUCCESS, result: res => console.log(`JsonResponse UserEmail: ${ res.results[0].email }`) },
+    { name: FETCH_FAILURE, result: err => console.log(`Error status code: ${ err.status }`) },
+    { name: ADD_TO_NUMBER, result: addToNumberResult(myNumber) }
 ];
 myEmitter.setEventEmitters(events);
 
-successFetch();
-errorFetch();
-myEmitter.emit('event', 'this is my event text');
+const successCallback = res => myEmitter.emit(FETCH_SUCCESS, res);
+const failureCallback = err => myEmitter.emit(FETCH_FAILURE, err);
+
+successFetch(successCallback, failureCallback);
+errorFetch(successCallback, failureCallback);
+
+const addToNumberCallback = () => myEmitter.emit(ADD_TO_NUMBER);
+
+successFetch(addToNumberCallback, null);
+errorFetch(null, addToNumberCallback);
